7ea291025d293b587f60fb925c102c84b65e43e2
reddytharun <reddytharun367@gmail.com>
2024-12-28 15:33:12 +0530

    1st commit

---
+++ src/main/java/sampleapp/SampleServlet.java
@@ -0,0 +1,18 @@
+package sampleapp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet("/sample")
+public class SampleServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        request.setAttribute("userName", name);
+        request.getRequestDispatcher("hello.jsp").forward(request, response);//
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Enter Your Name</h1>
+    <form action="sample" method="post">
+        <label for="name">Name: </label>
+        <input type="text" id="name" name="name" required>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
